cc9982ab2d7e354e07abeda3f052771ff411d467
emottt <31847517+emottt@users.noreply.github.com>
2022-02-19 12:41:38 +0200

    Add files via upload

---
+++ MovieStar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_Star
+{
+    [Serializable]
+    public class MovieStar
+    {
+        public DateTime dateOfBirth { get; set; }
+        
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Nationality { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Movie_Star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = movieStar();
+            if(results != null)
+            {
+                for (int i = 0; i < results.Count; i++)
+                {
+                    var birthday = results[i].dateOfBirth;
+
+                    var age = DateTime.Today.Year - birthday.Year;
+
+                    if(birthday.Date > DateTime.Today.AddYears(-age))
+                    {
+                        age--;
+                    }
+                    Console.WriteLine($"{results[i].Name} {results[i].SurName}\n" +
+                        $"{results[i].Sex}\n" +
+                        $"{results[i].Nationality}\n" +
+                        $"{age} years old\n");
+                }
+            }
+        }
+
+        static List<MovieStar> movieStar()
+        {
+            string fileName = @"G:\download\Test\Task One\JSON\movieStar.json";
+
+            if (File.Exists(fileName))
+            {
+                var movieStar = JsonConvert.DeserializeObject<List<MovieStar>>(File.ReadAllText(fileName));
+
+                return movieStar;
+            }
+
+            return null;
+        } 
+    }
+}
